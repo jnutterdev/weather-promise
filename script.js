d018303a19300e7b795b2343f7c24fcbdb794ab2
terribledevice <longlost@gmail.com>
2020-10-10 22:22:39 -0400

    getting into promises

--- script.js
+++ script.js
@@ -1,49 +1,8 @@
-const apikey = "a43c159fd95101abcda9aa38c075c205";
 const countrycode = 'USA';
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// update_ui = (info) => {
-//     document.getElementById('weather-results').innerHTML = JSON.stringify(info);
-// };
-
-// create_results = () => { document.getElementById("results-column").innerHTML = "Weather results" };
-
-// show_error = (error) => { console.error('something went wrong'); };
-
 // get_location_url = (zipcode, apikey) => {
 //     return `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode},${countrycode}&appid=${apikey}`;
 // }
-
-// let weather = new Promise(function(resolve, reject) {
-//     resolve("Done.");
-//     reject(show_error);
-// });
-
-// document.addEventListener('click', function() {
-//     get_weather(function(weather) {
-//         update_ui(weather);
-//     }, show_error)
-// });
